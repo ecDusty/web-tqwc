1c10093739f6b8ef73192f7eb7f86a37f99113f3
Erin Macalligan <github@unlikelyit.com>
2020-05-27 22:32:43 +0800

    ADD: working scroll position

--- app/components/home-page/scrollLady.js
+++ app/components/home-page/scrollLady.js
@@ -1,2 +1,7 @@
 const ladyWrap = document.querySelector('.js-drill-lady');
 const scrollIndicator = document.querySelector('.Index-page-scroll-indicator');
+
+if (ladyWrap && scrollIndicator) {
+	scrollIndicator.innerHtml = '';
+	scrollIndicator.appendChild(ladyWrap);
+}
